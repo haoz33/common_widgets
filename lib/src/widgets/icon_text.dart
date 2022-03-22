6d6e1af05ef4d555afcf5b485e2ab68e5df2f644
haoz33 <z513873698@gmail.com>
2022-03-22 15:17:19 +0000

    k

---
+++ lib/src/widgets/icon_text.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class IconText extends StatelessWidget {
+  const IconText({Key? key, required this.text, required this.icon})
+      : super(key: key);
+  final String text;
+  final Icon icon;
+  @override
+  Widget build(BuildContext context) {
+    return RichText(
+      text: TextSpan(
+        children: [
+          TextSpan(text: text, style: Theme.of(context).textTheme.bodyText1),
+          WidgetSpan(
+              child: Icon(
+            icon.icon,
+            size: 18,
+            color: icon.color,
+          )),
+        ],
+      ),
+    );
+  }
+}
